179a4d5d93e843a8d58fc88e597ad3590148d124
〖ЖƬ𝝙〗Ξɧєяя ➠VσιɖᏕє¢ᴸˣˣᴵ ! 〚Я₭₴〛 <s.olarcorps4@gmail.com>
2024-10-18 15:17:52 +0300

    Create ifstatement.cpp

---
+++ ifstatement.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  if (500 > 1) { // the actual if statement (real)
+std::cout << "nigga\n"; // nigga
+  }
+return 0; // not required but is gud habit to keep there :D
+}
