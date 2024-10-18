c76757e0e0835ca5b0860b5674181bcaa9403e25
〖ЖƬ𝝙〗Ξɧєяя ➠VσιɖᏕє¢ᴸˣˣᴵ ! 〚Я₭₴〛 <s.olarcorps4@gmail.com>
2024-10-18 15:53:10 +0300

    Create real.c

---
+++ real.c
@@ -0,0 +1,7 @@
+// did yk c code works on c++ files cuz technically c++ is like a dlc type shit
+#include <stdio.h> // still :(
+
+int main() {
+    printf("real\n");
+    return 0; // not required but is gud habit :D
+}
