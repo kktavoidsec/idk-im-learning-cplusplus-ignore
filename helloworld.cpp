d74d0df56a94489f8c7c51e693ba7dfcc194b686
〖ЖƬ𝝙〗Ξɧєяя ➠VσιɖᏕє¢ᴸˣˣᴵ ! 〚Я₭₴〛 <s.olarcorps4@gmail.com>
2024-10-18 15:08:15 +0300

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // wow guys you need an entire library to print shit :skull:
+
+int main() {
+std::cout << "hello world !!!\n";
+return 0; // not required but is gud habit to do :D
+}
